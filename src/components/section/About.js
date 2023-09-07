3eddf42bc5297440371797447a5acba77ae54891
Omr50 <omrab4@gmail.com>
2023-09-07 00:30:19 -0400

    first commit

---
+++ src/components/section/About.js
@@ -0,0 +1,65 @@
+    import React from 'react'
+    import Skills from '../layouts/Skills'
+    import { about, languages, frontend, backend, db_cloud_container, section2title } from '../../profile'
+    import serverGif from '../../styles/server.gif'
+    import webGif from '../../styles/webGif.gif'
+    import dbGif from '../../styles/dbGif.gif'
+
+    const About = () => {
+        return (
+            <div id="about" className="effect2">
+                <div data-aos="zoom-in-up" data-aos-once="true" className="row">
+                <div className="col-12 d-none d-md-block offset-md-1 col-md-4 Photo" id="not-dark2"></div>
+                <div className="col-12 offset-md-1 col-md-6 about">
+                    <div className="About-title-box">
+                    <h1 id="About" className="red-line">{section2title}</h1>
+                    </div>
+                    <p className="lead about-text">
+                        {about.paragraph}  
+                    </p>
+                </div>
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div id="Skills"> 
+
+                    <div className='row d-flex justify-content-left' style={{fontSize: '20px', fontWeight: '600', position: 'relative'}}>
+                        <img style={{width: 'fit-content', height: 'fit-content', position: 'inherit', left: '0px', top: '-10px'}} src={serverGif}></img>
+                        Backend 
+                    </div>
+                    <div className="row d-flex justify-content-center skills">
+                        {backend.map((x) => 
+                            <Skills key={x.name} faClass={x.faClass} svg={x.svg} label={x.name} source={x.source}/>
+                        )}
+                    </div> 
+
+                    <div className='row d-flex justify-content-left' style={{fontSize: '20px', fontWeight: '600', position: 'relative'}}>
+                    <img style={{width: 'fit-content', height: 'fit-content', position: 'inherit', left: '0px', top: '-10px'}} src={webGif}></img>
+                        Frontend
+                    </div>
+                    <div className="row d-flex justify-content-center skills">
+                        {frontend.map((x) => 
+                            <Skills key={x.name} faClass={x.faClass} svg={x.svg} label={x.name} source={x.source}/>
+                        )}
+                    </div> 
+
+                    <div className='row d-flex justify-content-left' style={{fontSize: '20px', fontWeight: '600'}}>
+                        <img style={{width: 'fit-content', height: 'fit-content', position: 'inherit', left: '0px', top: '-10px'}} src={dbGif}></img>
+                        Database, Cloud & Container Technologies
+                    </div>
+                    <div className="row d-flex justify-content-center skills">
+                        {db_cloud_container.map((x) => 
+                            <Skills key={x.name} faClass={x.faClass} svg={x.svg} label={x.name} source={x.source}/>
+                        )}
+                    </div> 
+
+                </div>
+
+            </div>
+
+        )
+    }
+
+    export default About
